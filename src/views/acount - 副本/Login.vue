e44365704176f595ff748d129e4bada4d11ba288
mituan <634646250@qq.com>
2020-11-19 15:31:43 +0800

    登录+人机校验

---
+++ src/views/acount - 副本/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="account">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+<style lang="scss">
+@import './style.scss';
+</style>
