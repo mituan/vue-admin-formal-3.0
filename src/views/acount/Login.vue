5508d42b565a91fef0987b54df372c6280d1cdd7
mituan <634646250@qq.com>
2020-11-23 10:52:13 +0800

    update

--- src/views/acount/Login.vue
+++ src/views/acount/Login.vue
@@ -2,7 +2,7 @@
     <div class="account">
         <div class="form-warp">
             <div class="fs-30 text-center">管理系统</div>
-            <a-form name='custom-validation' ref="ruleForm" v-bind="formConfig.layout">
+            <a-form name='custom-validation' ref="ruleForm" v-bind="layout">
                 <a-form-item>
                     <label>用户名</label>
                     <a-input type='text' autocomplete='off' />
@@ -12,24 +12,28 @@
                     <a-input type='password' autocomplete='off' />
                 </a-form-item>
                 <a-form-item>
-                    <div id="captcha-dom" class="nc-container" ></div>
+                   <Captcha></Captcha>
                 </a-form-item>
                 <a-form-item>
                     <a-button type="primary" html-type='submit' block> 登 录 </a-button>
                 </a-form-item>
             </a-form>
+             <div class="text-center">
+                <a class="color-white">忘记密码</a>|
+                <a class="color-white">注册</a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {
-        onMounted,
-        reactive
-    } from 'vue';
-    import './captcha.js';
+    import { onMounted, reactive, toRefs, ref } from 'vue';
+    import Captcha from '@/components/Captcha/index.vue';
     export default {
         name: 'Login',
+        components:{
+            Captcha
+        },
         setup(props) {
             const formConfig = reactive({
                 layout: {
@@ -41,53 +45,10 @@
                     },
                 },
             })
-
-
-            onMounted(() => {
-                var nc_token = ["FFFF0N00000000009931", (new Date()).getTime(), Math.random()].join(
-                ':'); // 没有做任何的响应
-                var NC_Opt = {
-                    renderTo: "#captcha-dom",
-                    appkey: "FFFF0N00000000009931",
-                    scene: "nc_login",
-                    token: nc_token,
-                    customWidth: 300,
-                    trans: {
-                        "key1": "code0"
-                    },
-                    elementID: ["usernameID"],
-                    is_Opt: 0,
-                    language: "cn",
-                    isEnabled: true,
-                    timeout: 3000,
-                    times: 5,
-                    apimap: {
-                        // 'analyze': '//a.com/nocaptcha/analyze.jsonp',
-                        // 'get_captcha': '//b.com/get_captcha/ver3',
-                        // 'get_captcha': '//pin3.aliyun.com/get_captcha/ver3'
-                        // 'get_img': '//c.com/get_img',
-                        // 'checkcode': '//d.com/captcha/checkcode.jsonp',
-                        // 'umid_Url': '//e.com/security/umscript/3.2.1/um.js',
-                        // 'uab_Url': '//aeu.alicdn.com/js/uac/909.js',
-                        // 'umid_serUrl': 'https://g.com/service/um.json'
-                    },
-                    callback: function (data) {
-                        window.console && console.log(nc_token)
-                        window.console && console.log(data.csessionid)
-                        window.console && console.log(data.sig)
-                    }
-                }
-                var nc = new noCaptcha(NC_Opt)
-                nc.upLang('cn', {
-                    _startTEXT: "请按住滑块，拖动到最右边",
-                    _yesTEXT: "验证通过",
-                    _error300: "哎呀，出错了，点击<a href=\"javascript:__nc.reset()\">刷新</a>再来一次",
-                    _errorNetwork: "网络不给力，请<a href=\"javascript:__nc.reset()\">点击刷新</a>",
-                })
-            })
+            const data = toRefs(formConfig);
 
             return {
-                formConfig
+                ...data,
             };
         },
     }
